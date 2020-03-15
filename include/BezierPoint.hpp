86d9a6c2da88052797d5254254d0686e860cc43c
Nathan Lichtlé <nathan.lichtle@gmail.com>
2020-03-15 16:42:03 +0100

    improve points design

--- include/BezierPoint.hpp
+++ include/BezierPoint.hpp
@@ -9,7 +9,7 @@ public:
         pos(pos), 
         front_handle(pos), 
         back_handle(pos),
-        pos_shape(radius),
+        pos_shape({2 * radius, 2 * radius}),
         front_handle_shape(radius),
         back_handle_shape(radius),
         front_handle_line(sf::Lines, 2),
@@ -18,14 +18,13 @@ public:
         display_front_handle(true),
         display_back_handle(true)
     {
-        for(auto* shape: {&pos_shape, &front_handle_shape, &back_handle_shape}) {
-            shape->setRadius(radius);
+        for(auto* shape: std::initializer_list<sf::Shape*>({&pos_shape, &front_handle_shape, &back_handle_shape})) {
             shape->setFillColor(sf::Color::White);
             shape->setOutlineColor(sf::Color::Blue);
             shape->setOutlineThickness(2.f);
             shape->setOrigin({radius, radius});
         }
-        pos_shape.setOutlineColor(sf::Color::Black);
+        pos_shape.setFillColor(sf::Color::Blue);
 
         pos_shape.setPosition(pos);
         front_handle_shape.setPosition(front_handle);
@@ -43,13 +42,17 @@ public:
 
     void set_display_front_handle(bool new_display) {
         display_front_handle = new_display;
+        pos_shape.setFillColor((display_front_handle && display_back_handle) ? 
+            sf::Color::Blue : sf::Color::White);
     }
     void set_display_back_handle(bool new_display) {
         display_back_handle = new_display;
+        pos_shape.setFillColor((display_front_handle && display_back_handle) ? 
+            sf::Color::Blue : sf::Color::White);
     }
     void set_display_handles(bool new_display) {
-        display_front_handle = new_display;
-        display_back_handle = new_display;
+        set_display_front_handle(new_display);
+        set_display_back_handle(new_display);
     }
 
     sf::Vector2f get_pos() {
@@ -81,7 +84,7 @@ public:
         _set_back_handle(new_pos);
     }
 
-    constexpr static float radius = 3.f;
+    constexpr static float radius = 4.f;
     
 private:
     void _set_back_handle(const sf::Vector2f& new_pos) {
@@ -115,7 +118,7 @@ private:
     bool display_front_handle;
     bool display_back_handle;
 
-    sf::CircleShape pos_shape;
+    sf::RectangleShape pos_shape;
     sf::CircleShape front_handle_shape;
     sf::CircleShape back_handle_shape;
 
